663d6cadde17d8768cf707bcd6bf41eb628b7fe8
Ronier Lima <ronier.lim@gmail.com>
2023-05-13 09:53:00 -0300

    aula

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,16 @@
-import { useEffect, useState } from "react";
-import Table from "./components/Table";
+import Routes from "./Routes";
+import Header from "./components/Header";
 import "./global.css";
 
-import axios from "axios";
-
-const ESTADO_DEFAULT = "CE";
-
 function App() {
-  const [estados, setEstados] = useState([]);
-  const [cidades, setCidades] = useState([]);
-
-  const [uf, setUf] = useState(ESTADO_DEFAULT);
-
-  useEffect(() => {
-    buscarEstados();
-  }, []);
-
-  useEffect(() => {
-    buscarCidades(uf);
-  }, [uf]);
-
-  async function buscarEstados() {
-    try {
-      const response = await axios.get(
-        `https://servicodados.ibge.gov.br/api/v1/localidades/estados`
-      );
-
-      setEstados(response.data);
-    } catch (error) {
-      console.error("Erro ao buscar estados:", error);
-      setEstados([]);
-    }
-  }
-
-  async function buscarCidades(estadoUF = ESTADO_DEFAULT) {
-    try {
-      const response = await axios.get(
-        `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${estadoUF}/municipios`
-      );
-
-      setCidades(response.data);
-    } catch (error) {
-      console.error(`Erro ao buscar cidades do estado ${estadoUF}:`, error);
-      setCidades([]);
-    }
-  }
-
   return (
-    <div>
-      <label htmlFor="estado">Estado:</label>
+    <>
+      <Header />
 
-      <select name="estado" id="estado" value={uf}>
-        {estados.map((estado) => (
-          <option key={estado.sigla} value={estado.sigla}>
-            {`${estado.nome} - ${estado.sigla}`}
-          </option>
-        ))}
-      </select>
+      <Routes />
 
-      <Table dados={cidades} />
-    </div>
+      <footer>sadasdasd</footer>
+    </>
   );
 }
 
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+function NotFound() {
+    return (
+      <div>
+        <h1>Não encontrada</h1>
+      </div>
+    );
+  }
+  
+  export default NotFound;
+  
